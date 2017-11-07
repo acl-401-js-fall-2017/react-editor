7cbb9412860a436680a3084062a749f6cfb07a32
Zac-L <Zachlichens@gmail.com>
2017-11-07 09:34:50 -0800

    creating a semi working form at the moment. going to work on getting a submit button working next

--- src/Form.js
+++ src/Form.js
@@ -9,13 +9,49 @@ export default class Form extends React.Component {
       password: '',
     }
 
+    change = e => {
+      this.setState({
+        [e.target.name]: e.target.value
+      });
+    }
+
+    onSubmit = () => {
+      console.log(this.state);
+    }
+
     render() {
       return (
         <form>
           <input 
+            name='firstName'
             placeholder='First name' 
             value={this.state.firstName}
-            onChange={event => this.setState({ firstName: event.target.value })}>
+            onChange={e => this.change(e)}>
+          </input>
+          <input 
+            name='lastName'
+            placeholder='Last name' 
+            value={this.state.lastName}
+            onChange={e => this.change(e)}>
+          </input>
+          <input 
+            name='userName'
+            placeholder='User name' 
+            value={this.state.userName}
+            onChange={e => this.change(e)}>
+          </input>
+          <input 
+            name='email'
+            placeholder='email' 
+            value={this.state.email}
+            onChange={e => this.change(e)}>
+          </input>
+          <input 
+            name='password'
+            type='password'
+            placeholder='password' 
+            value={this.state.password}
+            onChange={e => this.change(e)}>
           </input>
         </form>
       );
