cb735638cc6143efe6ec49bbd94e8c0154406391
ckrountree <jmail0207@gmail.com>
2017-11-06 22:56:48 -0800

    create Greeting Component class file

---
+++ src/Greeting.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Greeting extends Component {
+  render() {
+    const { salutation, statement, color } = this.props;
+
+    return (
+      <div style={{ color }} >
+        <span>
+          {salutation.toUpperCase()}
+        </span>
+        <span>
+          {statement}
+        </span>
+      </div>
+    );
+  }
+}
+
+export default Greeting;
