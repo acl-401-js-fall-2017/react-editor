05e44c56fac2ea19a61d9790ae90c624b9d9ea7c
Shane Moyo <smoyo@pdx.edu>
2017-11-07 13:07:13 -0800

    made some components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -9,12 +8,13 @@ class App extends Component {
     this.state = {
       salutation: 'Hello', 
       name: 'Defaut name',
-      color: '#fffff'
+      color: '#fffff',
+      comment: 'Leave a super cool comment!!!'
     }
   }
 
   handleSalutationChange(value){
-    this.setState({ salutaion: value});
+    this.setState({ salutation: value});
   }
 
   handleNameChange(value){
@@ -24,18 +24,57 @@ class App extends Component {
   handleColorChange(value){
     this.setState({ color: value})
   }
+  handeleCommentChange(value){
+    this.setState({ comment: value})
+  }
 
 
   render() {
 
-    const { salutation, name, color } = this.state;
+    const { salutation, name, color, comment } = this.state;
     return (
       <div className="App">
-        <Controls salutation={salutation} name={name}
+        <Controls salutation={salutation} name={name} comment={comment}
           onSalutationChange={salutation => this.handleSalutationChange(salutation)}
           onNameChange={name => this.handleNameChange(name)}
           onColorChange={color => this.handleColorChange(color)}
+          onCommentChange={comment => this.handeleCommentChange(comment)}
         />
+        <Greeting salutation={salutation}
+          color={color} name={name} specialClass="weird"
+        />
+        <Comment comment={comment} color={color}
+        />
+      </div>
+    );
+  }
+}
+
+class Controls extends Component {
+  render(){
+    const { salutation, name, color, comment, onSalutationChange, onNameChange, onColorChange, onCommentChange} = this.props;
+    return (
+      <div>
+        <label>
+          salutation:
+          <input name="salutation" value={salutation}
+            onChange={({ target }) => onSalutationChange(target.value)}/>
+        </label>
+        <br/>
+        <label>
+          name: <input name="name" value={name}
+            onChange={({ target }) => onNameChange(target.value)}/>
+        </label>
+        <br/>
+        <label>
+          color: <input name="color" type="color" value={color}
+            onChange={({ target }) => onColorChange(target.value)}/>
+        </label>
+        <br/>
+        <label>
+          comment: <input name="comment" value={comment}
+            onChange={({ target }) => onCommentChange(target.value)}/>
+        </label>
       </div>
     );
   }
@@ -43,7 +82,26 @@ class App extends Component {
 
 class Greeting extends Component {
   render(){
-    const { salutation, name, color, onSalutationChange, onNameChange, onColorChange} = this.props;
+    const { salutation, name, color, specialClass } = this.props;
+
+    return (
+      <div style={{ color }} className={specialClass}>
+        <span>{salutation.toUpperCase()}</span>
+        <span>{name}</span>
+      </div>
+    );
+  }
+}
+
+class Comment extends Component {
+  render(){
+    const { color, comment } =this.props;
+
+    return (
+      <div style={{ color }}>
+        <span>{comment}</span>
+      </div>
+    );
   }
 }
 
