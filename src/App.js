a95c8caef978958b4b3db79b2e5240d93db6fb07
mirrorapps <eli.speigel@gmail.com>
2017-11-07 11:21:08 -0800

    responsive input

--- src/App.js
+++ src/App.js
@@ -3,16 +3,86 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      firstName: 'John',
+      lastName: 'Smith',
+      bgColor: '#ffffff',
+      color: '#000000',
+      message: 'Come one, come all'
+    };
+  }
+
+  handleFirstNameChange(value) {
+    this.setState({ firstName: value });
+  }
+
+  handleLastNameChange(value) {
+    this.setState({ lastName: value });
+  }
+
+  handleMessage(value) {
+    this.setState({ message: value });
+  }
+
+  handleBGColorChange(value) {
+    this.setState({ bgColor: value });
+  }
+
+  handleColorChange(value) {
+    this.setState({ color: value });
+  }
+
+
   render() {
+    const { firstName, lastName, message, bgColor, color } = this.state;
+    
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Controls firstName={firstName} lastName={lastName} message={message}
+          onFirstNameChange={firstName => this.handleFirstNameChange(firstName)}
+          onLastNameChange={lastName => this.handleLastNameChange(lastName)}
+          onMessageChange={message => this.handleMessageChange(message)}
+          onBGColorChange={bgColor => this.handleBGColorChange(bgColor)}
+          onColorChange={color => this.handleColorChange(color)}
+        />
+        
+      </div>
+    );
+  }
+}
+
+class Controls extends Component {
+  render () {
+    const { firstName, lastName, message, bgColor, color, onFirstNameChange, onLastNameChange, onMessageChange, onBGColorChange, onColorChange } = this.props;
+    return (
+      <div>
+        <label>
+          firstName:
+          <input name="firstName" value={firstName}
+            onChange={({ target }) => onFirstNameChange(target.value)}/>
+        </label>
+        <label>
+          lastName:
+          <input name="lastName" value={lastName}
+            onChange={({ target }) => onLastNameChange(target.value)}/>
+        </label>
+        <label>
+          message:
+          <input name="message" value={message}
+            onChange={({ target }) => onMessageChange(target.value)}/>
+        </label>
+        <label>
+          bgColor:
+          <input name="bgColor" value={bgColor}
+            onChange={({ target }) => onBGColorChange(target.value)}/>
+        </label>
+        <label>
+          color:
+          <input name="color" value={color}
+            onChange={({ target }) => onColorChange(target.value)}/>
+        </label>
       </div>
     );
   }
