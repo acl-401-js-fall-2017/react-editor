3f962875e0b08f9db7ffed9f31640f63e29a0415
Zac-L <Zachlichens@gmail.com>
2017-11-07 13:02:18 -0800

    add constructor and on change function to handle field state. Tried out spread operator

--- src/App.js
+++ src/App.js
@@ -4,10 +4,32 @@ import './App.css';
 import Form from './Form';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      fields: {}
+    };
+  }
+
+  onChange = updatedValues => {
+    this.setState({ 
+      fields: {
+        ...this.state.fields,
+        ...updatedValues
+      }
+    });
+  }
+
   render() {
+    const { fields } = this.state;
+
     return (
       <div className="App">
-        <Form />
+        <Form onChange={fields => this.onChange(fields)} />
+        <p>
+          {JSON.stringify(fields, null, 2)}
+        </p>
       </div>
     );
   }
